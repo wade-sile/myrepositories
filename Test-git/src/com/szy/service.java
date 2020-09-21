d2ce67aa2c1cd6e8a3368aefd8e921f9e529e07a
wade-sile <940694710@qq.cim>
2020-09-21 17:49:12 +0800

    hh

--- Test-git/src/com/szy/service.java
+++ Test-git/src/com/szy/service.java
@@ -4,6 +4,7 @@ public class service {
 	public static void main(String[] args) {
 		System.out.println("2");
 		System.out.println("1");
+		System.out.println(1/0);
 		System.out.println("szpt");
 		System.out.println("cnm");
 	}
